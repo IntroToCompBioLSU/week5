3e17a4c628e1b5b1cf1f864e0fb31e17c080e9c3
jembrown <jeremymbrown@gmail.com>
2018-12-02 14:11:46 -0600

    Added comments for week 5 assignment

--- assignment.sh
+++ assignment.sh
@@ -3,12 +3,16 @@
 echo "Enter filename"
 read fname
 
+# DB: How could you accept this filename (or many file names) as command-line arguments?
+
 if [ ! -f "$fname" ]; then
     echo "No such file!" >&2
     exit 1
 fi
 
-words=$( wc -w <"$fname" )
+words=$( wc -w <"$fname" )  # DB: Interesting, I'm not familiar with this syntax for reading from files.
 
 printf 'The file "%s" has %d words. \n' \
-	"$fname" "$words"
+	"$fname" "$words"  # I also didn't realize you could do this kind of string formatting in bash.
+
+# DB: For loop?
