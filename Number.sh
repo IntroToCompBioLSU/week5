a1d41e9e8cedd6d76f79b40f9cdd797f32a40d66
jembrown <jeremymbrown@gmail.com>
2018-12-02 23:01:20 -0600

    Comments added

--- Number.sh
+++ Number.sh
@@ -19,3 +19,11 @@ if [ $@ -eq 55 ]
 then
         echo "YOU HAVE GUESSED THE RIGHT NUMBER"
 fi
+
+# DB: I think this is a good use of if/else statements. Just two things:
+
+# (1) $@ is really meant for a whole list of command-line arguments. If you are expecting
+#     just one number, you could use $1.
+
+# (2) If you wrap this all in a while loop, you can have the user keep entering a number
+#     until they guess correctly, without having to call the script multiple times.
--- WillieScript.sh
+++ WillieScript.sh
@@ -6,3 +6,8 @@ for welcome in $@
 do
 	echo "Welcome $welcome. See if you can guess the secrete number. Think of a number between 0-100, then enter that number by typing in './Number.sh (X)' where (X) is your number. Keep trying till ya' get it right! Good luck. "
 done
+
+# DB: Good to see the for loop here. You could combine this with your number script by 
+# prompting the user to enter a number (or numbers) after seeing the welcome message and
+# then checking to see if they're right. You could even use a while loop to keep going
+# until they guess the right number.
