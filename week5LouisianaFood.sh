69851e1aa6f6c8d834989f838e4443117172fca1
jembrown <jeremymbrown@gmail.com>
2018-12-06 14:58:35 -0600

    Comments added

--- week5LouisianaFood.sh
+++ week5LouisianaFood.sh
@@ -11,8 +11,13 @@ else
 	echo "$@ is not as satisfying as jambalaya."
 fi
 
+# DB: This is creative! But the test will break if you give it 0 or more than 1 command-line
+#     arguments. How could you use a for loop to accept any number of arguments?
+
 #For loop
 
 for food in crawfish boudin gumbo redbeans
 do echo $food "is another yummy Louisiana delicacy."
 done
+
+# DB: So hungry now...
